4b9b64656a7b0af3bd4a28b24f813649f9993e3f
Quzeem <quzeemagbaje@gmail.com>
2020-09-10 14:52:00 +0100

    Work on GeoJSON Location, Slug, Geospatial Query, and Advanced Querying

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,15 +1,74 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+  let query;
+  // Copy req.query(query object) into reqQuery
+  const reqQuery = { ...req.query };
+  // Things to exclude from fields to match for filtering
+  const excludeFields = ['select', 'sort'];
+  // Loop over excludeFields and delete them from reqQuery(request query)
+  excludeFields.forEach((param) => delete reqQuery[param]);
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+  // Create mongoDB operators ($gt, $gte, etc.)
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+  // Select Fields from document(s) --- Bootcamp.find().select()
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+  // Sort Documents by field(s) --- Ascending(1) or Descending(-1)
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    // default sort using createdAt in descending order
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 50;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const totalDoc = await Bootcamp.countDocuments();
+  query = query.skip(startIndex).limit(limit);
+  // Executing query
+  const bootcamps = await query;
+  // Pagination Result
+  const pagination = {};
+  // show/hide next logic
+  if (endIndex < totalDoc) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  // show/hide prev logic
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  // Send response
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc    Get a bootcamp
@@ -72,4 +131,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Public
+exports.getBootcampsWithinRadius = asyncHandler(async (req, res, next) => {
+  // Get zipcode and distance from user
+  const { zipcode, distance } = req.params;
+
+  //  Get long and lat by geocoding
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const long = loc[0].longitude;
+
+  // Calc radius using radians --> radius(r) = distance(d) / radius of the earth(R)
+  // R = 3,963 mi(in miles) or 6,378 km(in kilometres)
+  // For our distance(d), we're assuming it's in miles.
+  const radius = distance / 3963;
+
+  // Get bootcamps within the radius calculated from tdistancehe specified
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[long, lat], radius] } },
+  });
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+mongoose.connect(process.env.DEV_LOCAL_MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+// Require Model
+const Bootcamp = require('./models/Bootcamp');
+
+// Read Data
+const bootcamps = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '_data', 'bootcamps.json'), 'utf-8')
+);
+
+// Import Data
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete Data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Run specific code
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
