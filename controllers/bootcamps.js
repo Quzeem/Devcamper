c322fbd0fe7f7667e2331ea2838d002d161c4126
Quzeem <quzeemagbaje@gmail.com>
2020-09-12 19:38:42 +0100

    Add course model, controllers, and routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -21,8 +21,10 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
+
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
+
   // Select Fields from document(s) --- Bootcamp.find().select()
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
@@ -121,7 +123,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(
@@ -130,6 +132,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
       )
     );
   }
+  // Trigger cascade deletion of associated documents
+  bootcamp.remove();
+
   res.status(200).json({ success: true, data: {} });
 });
 
